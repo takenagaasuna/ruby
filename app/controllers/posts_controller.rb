70d36afbf9232f2723d4eecc86031e8b8d03a8b2
takenagaasuna <atake0847@gmail.com>
2018-08-21 06:21:28 +0000

    rails21_9delate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,15 +9,37 @@ class PostsController < ApplicationController
     end
     
     def new
+        @post=Post.new
     end
+    
     def create
         #render plain: params[:post].inspect
         @post = Post.new(post_params)
         if @post.save
-            redirent_to posts_path
+            redirect_to posts_path
         else
-            render plain: @post.errors.inspect
+            #render plain: @post.errors.inspect
+            render 'new'
         end
+    end
+    
+    def edit
+        @post=Post.find(params[:id])
+    end
+    
+    def update
+        @post=Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to posts_path
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @post=Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
     end
     
     private
