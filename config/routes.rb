72da7401cf42d5a9879bdd255d978108092317e3
takenagaasuna <atake0847@gmail.com>
2018-08-22 03:55:22 +0000

    rails22_2login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     resources :posts do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_005745) do
+ActiveRecord::Schema.define(version: 2018_08_22_013320) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2018_08_22_005745) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
