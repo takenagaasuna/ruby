1e474e123cd06a6539589f7919ef5ada534eb4e5
takenagaasuna <atake0847@gmail.com>
2018-08-22 01:22:26 +0000

    rails22_1pic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_062501) do
+ActiveRecord::Schema.define(version: 2018_08_22_005745) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -20,11 +20,20 @@ ActiveRecord::Schema.define(version: 2018_08_21_062501) do
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
+  create_table "pictures", force: :cascade do |t|
+    t.string "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_pictures_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
 end
